332e2b74177294ecfa0a4343fca453f26dc6c42a
Ayush soni <99383813+546-Ayush@users.noreply.github.com>
2023-05-31 11:57:44 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,97 @@
+{
+    "topPicks"[
+       {
+          "category":"veg",
+          "image_url":"",
+          "item_name":"Biryani",
+          "rating":"4.2"
+       },
+       {
+          "category":"veg",
+          "image_url":"",
+          "item_name":"Biryani",
+          "rating":"4.2"
+       },
+       {
+          "category":"non-veg",
+          "image_url":"",
+          "item_name":"Biryani",
+          "rating":"4.2"
+       },
+       {
+          "category":"veg",
+          "image_url":"",
+          "item_name":"Biryani",
+          "rating":"4.2"
+       },
+       {
+          "category":"non-veg",
+          "image_url":"",
+          "item_name":"Biryani",
+          "rating":"4.2"
+       },
+       {
+          "category":"veg",
+          "image_url":"",
+          "item_name":"Biryani",
+          "rating":"4.2"
+       }
+    ],
+    "cards"[
+       {
+          "offer":"50",
+          "image":"",
+          "category":"veg",
+          "itemName":"Mix Veg",
+          "description":"Served with 2 leg piece, gravy and 1 bowl",
+          "rating":"4.5",
+          "views":"12.2",
+          "originalPrice ":"50",
+          "currentPrice":"47.99"
+       },
+       {
+          "offer":"50",
+          "image":"",
+          "category":"non-veg",
+          "itemName":"Mix Veg",
+          "description":"Served with 2 leg piece, gravy and 1 bowl",
+          "rating":"4.5",
+          "views":"12.2",
+          "originalPrice ":"50",
+          "currentPrice":"47.99"
+       },
+       {
+          "offer":"50",
+          "image":"",
+          "category":"veg",
+          "itemName":"Mix Veg",
+          "description":"Served with 2 leg piece, gravy and 1 bowl",
+          "rating":"4.5",
+          "views":"12.2",
+          "originalPrice ":"50",
+          "currentPrice":"47.99"
+       },
+       {
+          "offer":"50",
+          "image":"",
+          "category":"non-veg",
+          "itemName":"Mix Veg",
+          "description":"Served with 2 leg piece, gravy and 1 bowl",
+          "rating":"4.5",
+          "views":"12.2",
+          "originalPrice ":"50",
+          "currentPrice":"47.99"
+       },
+       {
+          "offer":"50",
+          "image":"",
+          "category":"veg",
+          "itemName":"Mix Veg",
+          "description":"Served with 2 leg piece, gravy and 1 bowl",
+          "rating":"4.5",
+          "views":"12.2",
+          "originalPrice ":"50",
+          "currentPrice":"47.99"
+       }
+    ]
+ }
